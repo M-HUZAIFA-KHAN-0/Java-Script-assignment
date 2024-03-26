ee3abac5c4f70808b64e0f2696cc70f6220a784f
Muhammad Huzaifa Khan <154981372+M-HUZAIFA-KHAN-0@users.noreply.github.com>
2024-03-27 02:02:45 +0500

    Add files via upload

---
+++ Assignment No 1/script.js
@@ -0,0 +1,12 @@
+let a = prompt("Enter Your First Name")
+let b = prompt("Enter Your Middle Name")
+let c = prompt("Enter Your last Name")
+
+
+console.log("Your First Name is" + " " + a )
+console.log("Your Middle Name is" + " " + b )
+console.log("Your last Name is" + " " + c )
+
+console.log(a + " "  + b + " " + c)
+
+
