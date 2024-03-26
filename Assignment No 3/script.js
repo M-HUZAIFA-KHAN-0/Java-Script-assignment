935432ad5e05f02bfde0b81bdde63c84dc89f62e
Muhammad Huzaifa Khan <154981372+M-HUZAIFA-KHAN-0@users.noreply.github.com>
2024-03-27 03:19:41 +0500

    Add files via upload

---
+++ Assignment No 3/script.js
@@ -0,0 +1,55 @@
+let percentage = prompt("Enter Your Percentage")
+let grade
+
+
+if(percentage > 100)
+{
+    console.log("Please Enter a Correct Percentage")
+}
+else if(percentage >= 90)
+{
+    console.log("A+")
+    grade = "A+"
+}
+else if(percentage >= 80)
+{
+    console.log("A1")
+    grade = "A1"
+}
+else if(percentage >= 70)
+{
+    console.log("A")
+    grade = "A"
+}
+else if(percentage >= 60)
+{
+    console.log("B")
+    grade = "B"
+}
+else if(percentage >= 50)
+{
+    console.log("C")
+    grade = "C"
+
+}
+else if(percentage >= 40)
+{
+    console.log("D")
+    grade = "D"
+
+}
+else if(percentage < 40)
+{
+    console.log("F")
+    grade = "F"
+}
+
+
+
+
+
+
+
+
+
+
